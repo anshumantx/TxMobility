32c69dc2fc34641730e8a96918cb577906085872
anshuman Singh <anshuman.singh@testingxperts.com>
2022-01-18 12:20:33 +0530

    initial commit

---
+++ js/function.js
@@ -0,0 +1,54 @@
+$(function() {
+    $('.scroll-to-section , nav ul li a, ul li a').click(function() {
+        if ($('#navbar').hasClass('in')) {
+            $('#navbar').removeClass('in');
+          $('#navbar').removeAttr('style');
+      }
+      $(this).addClass("select");
+      $(this).parent().siblings().find("a").removeClass("select");
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top -65
+          }, 1800);
+          return false;
+        }
+      }
+    });
+  });
+  
+
+  var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter-value').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 4000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
+});
